f43e1ef054ec5519f38c42988835a7e8f75535c6
johan <johan.chandra.putra@gmail.com>
2021-10-12 07:18:16 +0700

    success routeing

---
+++ router/gameRPS.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/gameRPS", (req, res) =>{
+    res.render("gameRPS");
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
-app.use("/", express.static("./public"));
+app.use(express.static("./public"));
 app.use(express.json());
-
 const PORT = 4000;
+
+const indexRouter = require("./router/");
+const gameRouter = require("./router/gameRPS");
+app.use("/", indexRouter);
+app.use("/", gameRouter);
+
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
 
 app.listen(PORT, ()=>{
     console.log(`Listening on ${PORT}`);
